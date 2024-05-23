0126381e5dba8a804169084b963d56af541f8c5a
marcelo.alves <marcelo.alves@pt.clara.net>
2024-05-23 15:46:01 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,36 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Request for a Raise",
-  description: "A simple request for a raise",
   openGraph: {
-    images: 'https://photos.sphereshowcase.com/tBJczsgyzUAP3woETDr31.jpg',
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
   },
 };
+
+// export const metadata: Metadata = {
+//   title: "Request for a Raise",
+//   description: "A simple request for a raise",
+//   openGraph: {
+//     images: 'https://photos.sphereshowcase.com/tBJczsgyzUAP3woETDr31.jpg',
+//   },
+// };
 
 export default function RootLayout({
   children,
